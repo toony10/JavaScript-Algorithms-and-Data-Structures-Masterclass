d13c888ab037c2166a70c7b74322e17a23be6a9d
toony10 <amiranton257@gmail.com>
2022-11-17 00:51:44 +0200

    multiple pointers

---
+++ problem solving patternes/multiple pointers/count-uniq-values.js
@@ -0,0 +1,15 @@
+// implement a function that called countUniqueValues that accepts a sort array and counts the unique value in the array.there can be nigative numbers in the array but it always be sorted
+
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+  let i = 0;
+  for (j = 0; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+countUniqueValues([1, 1, 1, 2, 3, 3, 4, 4, 5, 6]);
---
+++ problem solving patternes/multiple pointers/multiple-pointers.js
@@ -0,0 +1,30 @@
+// write a function that acceptes a sorted array of integers. the function should find the first pair where the sum is 0. return an arrau that includes both values that sum is zero or undefined if apair does not exist
+
+// Naive Solution
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+
+// Refactor
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+sumZero([-3, -2, -1, 0, 1, 2, 3]);
