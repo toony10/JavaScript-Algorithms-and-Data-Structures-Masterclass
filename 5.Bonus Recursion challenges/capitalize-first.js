641a8ba82139c6119039a67f9517d75afe6a1dc6
toony10 <amiranton257@gmail.com>
2022-12-24 02:09:44 +0200

    bonus recursion challenges - part2

---
+++ 5.Bonus Recursion challenges/capitalize-first.js
@@ -0,0 +1,15 @@
+// Write a recursive function called capitalizeFirst. Given an array of strings, capitalize the first letter of each string in the array.
+
+function capitalizeFirst(words) {
+  // add whatever parameters you deem necessary - good luck!
+  let newWords = [];
+  if (words.length === 0) {
+    return newWords;
+  }
+  let captlize = words[0].charAt(0).toUpperCase() + words[0].slice(1);
+  newWords.push(captlize);
+  newWords = newWords.concat(capitalizeFirst(words.slice(1)));
+  return newWords;
+}
+
+capitalizeFirst(["car", "taco", "banana"]); // ['Car','Taco','Banana']
