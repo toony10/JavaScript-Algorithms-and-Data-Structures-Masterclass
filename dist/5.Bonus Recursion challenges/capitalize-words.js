6576d6e60b1df99be927c334e3c48c620f826c03
toony10 <amiranton257@gmail.com>
2025-07-17 22:49:58 +0300

    convert all fils to type script

---
+++ dist/5.Bonus Recursion challenges/capitalize-words.js
@@ -0,0 +1,14 @@
+"use strict";
+// Write a recursive function called capitalizeWords. Given an array of words, return a new array containing each word capitalized.
+function capitalizeWords(words) {
+    let newWords = [];
+    if (words.length === 0) {
+        return newWords;
+    }
+    let captlize = words[0].toUpperCase();
+    newWords.push(captlize);
+    newWords = newWords.concat(capitalizeWords(words.slice(1)));
+    return newWords;
+}
+// let words = ['i', 'am', 'learning', 'recursion'];
+// capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
